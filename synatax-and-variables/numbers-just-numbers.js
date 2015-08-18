50cb0c403e28c205e1a8ae3e0449b308486df483
keith.barrow <keith.barrow@tombola.com>
2015-08-18 11:44:43 +0100

    some examples

--- synatax-and-variables/numbers-just-numbers.js
+++ synatax-and-variables/numbers-just-numbers.js
@@ -1,6 +1,8 @@
 var foo = 42;
 var bar = 42.0;
 var baz = 0x2A;
+var qux = 'waldorf' * 2;
+
 //=== means same type and value
 console.log(foo===bar);
 console.log(foo===baz);
@@ -9,3 +11,7 @@ console.log(foo===baz);
 var x =  0.1 * 0.2;
 console.log("0.1 * 0.2 = ", x);
 console.log("float fail!");
+
+
+console.log('"waldorf" // 2 =', qux);
+console.log('is NaN === NaN' + qux === qux);
