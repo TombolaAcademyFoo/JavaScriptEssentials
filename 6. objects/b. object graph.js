15794e9fa365c56892d31471b08d078239ab1f3e
keith.barrow <keith.barrow@tombola.com>
2015-08-27 15:36:48 +0100

    Added object graph stuff

---
+++ 6. objects/b. object graph.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict';
+    var Foo = function (){
+            this.billy = function (){
+
+            };
+        },
+        bar = new Foo();
+    console.log('put breakpoint here');
+})();
